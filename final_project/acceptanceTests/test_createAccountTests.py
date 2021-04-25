7f462366a877b8923a5f1713fe52b16af6437cb4
mnkoc <mnkocher@uwm.edu>
2021-04-24 23:09:10 -0500

    acceptance tests

--- final_project/acceptanceTests/test_createAccountTests.py
+++ final_project/acceptanceTests/test_createAccountTests.py
@@ -1,10 +1,29 @@
-import unittest
+from django.test import TestCase
+from django.test import Client
+from final_project.models import User
 
+class CreateAccountTestClass(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user1 = User.objects.create(email='user@gmail.com', password='1234', phoneNumber=1234567890)
+        self.user2 = User.objects.create(email='anotheruser@gmail.com', password='5678', phoneNumber=1234567890)
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def testNoUserName(self):
+        resp = self.client.post("/", {'email': '', 'password': '5678'})
+        self.assertEqual(resp.context["message"], "user did not enter username", msg="Invalid username entered did not result in error message")
 
+    def testValidUsername(self):
+        resp = self.client.post('/', {'email': 'user@gmail.com', 'password': '1234'})
+        self.assertEqual(resp.url, '/dashboard/', msg='Username accepted')
 
-if __name__ == '__main__':
-    unittest.main()
+    def testOtherUserName(self):
+        resp = self.client.post("/", {'email': 'user@gmail.com', 'password': '5678'})
+        self.assertEqual(resp.context["message"], "bad password", msg="Another user's email entered did not result in error message")
+
+    def testNoPassword(self):
+        resp = self.client.post('/', {'email': 'user@gmail.com', 'password': ''})
+        self.assertEqual(resp.context["message"], "bad password", msg="No password entered did not result in error message")
+
+    def testValidPassword(self):
+        resp = self.client.post('/', {'email': 'user@gmail.com', 'password': '1234'})
+        self.assertEqual(resp.url, '/dashboard/', msg='User account created')
