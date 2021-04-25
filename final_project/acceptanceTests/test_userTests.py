4c97c37fe9b73e3a1afee654c444d5f44856057a
kate <jankow72@uwm.edu>
2021-04-25 14:32:49 -0500

    added comments to the user test cases

--- final_project/acceptanceTests/test_userTests.py
+++ final_project/acceptanceTests/test_userTests.py
@@ -9,23 +9,32 @@ class UserLoginTestClass(TestCase):
         self.user1 = User.objects.create(email='user@gmail.com', password='1234', phoneNumber=1234567890)
         self.user2 = User.objects.create(email='anotheruser@gmail.com', password='5678', phoneNumber=1234567890)
 
+    # test no password entered
     def test_noPassword(self):
         resp = self.client.post('/', {'email': 'user@gmail.com', 'password': ''})
-        self.assertEqual(resp.context["message"], "bad password", msg="No password entered did not result in error message")
+        self.assertEqual(resp.context["message"], "bad password",
+                         msg="No password entered did not result in error message")
 
+    # test other user's password
     def test_otherUserPassword(self):
         resp = self.client.post("/", {'email': 'user@gmail.com', 'password': '5678'})
-        self.assertEqual(resp.context["message"], "bad password", msg="Another user's password entered did not result in error message")
+        self.assertEqual(resp.context["message"], "bad password",
+                         msg="Another user's password entered did not result in error message")
 
+    # test altogether the wrong password
     def test_wrongPassword(self):
         resp = self.client.post("/", {'email': 'user@gmail.com', 'password': 'hello'})
-        self.assertEqual(resp.context["message"], "bad password", msg="Incorrect password entered did not result in error message")
+        self.assertEqual(resp.context["message"], "bad password",
+                         msg="Incorrect password entered did not result in error message")
 
+    # test invalid username
     def test_noUser(self):
         resp = self.client.post("/", {'email': 'myemail@gmail.com', 'password': '5678'})
-        self.assertEqual(resp.context["message"], "user does not exist", msg="Invalid username entered did not result in error message")
+        self.assertEqual(resp.context["message"], "user does not exist",
+                         msg="Invalid username entered did not result in error message")
 
+    # test valid login
     def test_validPassword(self):
         resp = self.client.post('/', {'email': 'user@gmail.com', 'password': '1234'})
-        self.assertEqual(resp.url, '/dashboard/', msg='User was not redirected to dashboard page after logging in with correct credentials')
-
+        self.assertEqual(resp.url, '/dashboard/',
+                         msg='User was not redirected to dashboard page after logging in with correct credentials')
